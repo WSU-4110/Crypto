149f091506417adc474d6b544270392cb7864191
Abdul Rashid <abdulr07@live.com>
2021-07-01 21:06:28 -0400

    Create FAQ Section and Add Routing for FAQ pages

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import { HeaderToolbar } from "./components/Header"
 import Profile from "./components/Profile"
 import { FirstPartyEducation } from "./components/FirstPartyEducation"
 import { ThirdPartyEducation } from "./components/ThirdPartyEducation"
+import Faq from './components/FAQ/Faq'
 
 function App() {
   return (
@@ -29,6 +30,7 @@ function App() {
               <PrivateRoute path="/profile" component={Profile} />
               <Route path="/firstpartyeducation"> <FirstPartyEducation /> </Route>
               <Route path="/thirdpartyeducation"> <ThirdPartyEducation /> </Route>
+              <Route path="/faq"> <Faq /> </Route>
               {/*  locking down home */}
               <PrivateRoute exact path="/" component={Home} />
             </Switch>
