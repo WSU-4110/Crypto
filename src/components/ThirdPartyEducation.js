73bae935a1fd7528d9f5251fce96dc8d6d110321
Sadeem Shamoun <sadeem23.sj@gmail.com>
2021-07-06 00:50:18 -0400

    Third Party Page

--- src/components/ThirdPartyEducation.js
+++ src/components/ThirdPartyEducation.js
@@ -3,6 +3,22 @@ import React from "react";
 export const ThirdPartyEducation = () => {
 
     return (
-        null
+        <div className="ThirdPartyEducation">
+            <form className="search" action ="">
+
+                <label for="News"/>
+                
+                <input className ="input" type="text" /><br />
+                
+                <input type="submit"/> 
+            </form>
+
+            <div className="container"/>
+                <ul className="news-list"/>
+            <div/>
+
+            <script type="text/javascript" src="./components/TPE.js"></script>
+
+        </div>
     )
 }
